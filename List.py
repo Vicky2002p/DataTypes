65a2887af66423352828912918b55e7862f6fe8d
Vicky2002p <93857836+Vicky2002p@users.noreply.github.com>
2022-06-10 21:33:27 -0400

    Update List.py
    changed the string to int

--- List.py
+++ List.py
@@ -9,7 +9,7 @@ print(f"Oh, so {fav_animal} is ur favorite animal")
 new_str = fav_animal.replace("O", "*")              
 print(f"How about we dont use the letter 'o' in this word! Then we have {new_str}")
 #Taking input and printing
-fav_num = input("Enter your 2 digit favorite number between 10 to 99: ")
+fav_num = int(input("Enter your 2 digit favorite number between 10 to 99: "))
 print(f"So, is {fav_num} your lucky number as well?")
 #Making a list and printing
 list1 = ['red','blue','yellow','green','red','pink',1,[2,3]]
@@ -18,7 +18,7 @@ print(list1)
 print(list1[2][-3:])
 print(list1[3:])
 #Adding the input on an index
-list1.insert(0, input("Enter a new colour"))
+list1.insert(0, input("Enter a new colour: "))
 print(f"The updated list looks like: {list1}")
 res = len(list1) - fav_num
 print(f"The number elements in the list is {len(list1)}")
